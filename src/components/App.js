6d45295b236b60a4e239ff9a3189a652b39212e0
“Andrew <andrewwoods88@gmail.com>
2019-02-06 15:59:13 -0500

    test deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-
 class App extends React.Component {
     render () {
         return (
-            <h1>Wecome to my boilder plate {this.props.website}</h1>
+            <div>
+                <h1>Wecome to my boilder plate {this.props.website}</h1>
+                <h2>second test deploy</h2>
+            </div>
         );
     }
 }
